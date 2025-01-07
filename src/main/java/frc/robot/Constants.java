aabb0661b1e54884496de504256e57ea77ef63b3
CHS Robotics <chsrobotics@gibdist.net>
2025-01-07 16:56:44 -0500

    Converted MAXSwerveModule to 2025

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,9 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 public final class Constants {
   public static final class OperatorConstants {
@@ -101,6 +103,48 @@ public final class Constants {
 
     public static final int kDrivingMotorCurrentLimit = 50; // amps
     public static final int kTurningMotorCurrentLimit = 20; // amps
+  }
+
+  public static final class SwerveConfig {
+    public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
+    public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
+
+    // Setup all the constants, using those defined above in ModuleConstants. Used in MAXSwerveModule.java
+    static {
+      drivingConfig
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(ModuleConstants.kDrivingMotorCurrentLimit);
+      drivingConfig.encoder
+        .positionConversionFactor(ModuleConstants.kDrivingEncoderPositionFactor) // meters
+        .velocityConversionFactor(ModuleConstants.kDrivingEncoderVelocityFactor); // meters per second
+      drivingConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        // These are example gains you may need to them for your own robot!
+        .pid(ModuleConstants.kDrivingP, ModuleConstants.kDrivingI, ModuleConstants.kDrivingD)
+        .velocityFF(ModuleConstants.kDrivingFF)
+        .outputRange(ModuleConstants.kDrivingMinOutput, ModuleConstants.kDrivingMaxOutput);
+
+      turningConfig
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(ModuleConstants.kTurningMotorCurrentLimit);
+      turningConfig.absoluteEncoder
+        // Invert the turning encoder, since the output shaft rotates in the opposite
+        // direction of the steering motor in the MAXSwerve Module.
+        .inverted(ModuleConstants.kTurningEncoderInverted)
+        .positionConversionFactor(ModuleConstants.kTurningEncoderPositionFactor) // radians
+        .velocityConversionFactor(ModuleConstants.kTurningEncoderVelocityFactor); // radians per second
+      turningConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+        // These are example gains you may need to them for your own robot!
+        .pid(ModuleConstants.kTurningP, ModuleConstants.kTurningI, ModuleConstants.kTurningD)
+        .outputRange(ModuleConstants.kTurningMinOutput, ModuleConstants.kTurningMaxOutput)
+        // Enable PID wrap around for the turning motor. This will allow the PID
+        // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
+        // to 10 degrees will go through 0 rather than the other direction which is a
+        // longer route.
+        .positionWrappingEnabled(true)
+        .positionWrappingInputRange(ModuleConstants.kTurningEncoderPositionPIDMinInput, ModuleConstants.kTurningEncoderPositionPIDMaxInput);
+    }
   }
 
   public static final class OIConstants {
